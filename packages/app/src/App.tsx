e66f6ada495ba520532dcefb28d41e0c910ee46e
Abhi232003 <khodadeabhishek@gmail.com>
2025-08-08 20:47:23 +0530

    OAuth setup and CI/CD monitoring

--- packages/app/src/App.tsx
+++ packages/app/src/App.tsx
@@ -24,6 +24,7 @@ import { apis } from './apis';
 import { entityPage } from './components/catalog/EntityPage';
 import { searchPage } from './components/search/SearchPage';
 import { Root } from './components/Root';
+import { githubAuthApiRef } from '@backstage/core-plugin-api';
 
 import {
   AlertDisplay,
@@ -56,7 +57,7 @@ const app = createApp({
     });
   },
   components: {
-    SignInPage: props => <SignInPage {...props} auto providers={['guest']} />,
+    SignInPage: props => <SignInPage {...props} auto providers={['guest', { id: 'github', title: 'GitHub', message: 'Sign in using GitHub', apiRef: githubAuthApiRef }]} />,
   },
 });
 
